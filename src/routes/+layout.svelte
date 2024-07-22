89077e469ecd474443626184277f7241fe9db0b0
Manassarn Manoonchai <manassarn@gmail.com>
2024-07-22 14:28:07 +0700

    Setup app layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
-<script>
+<script type="ts">
 	import '../app.css';
 </script>
 
-<slot></slot>
+<div class="grid h-dvh grid-rows-[auto_1fr_auto]">
+	<!-- Header -->
+	<header class="sticky top-0 z-10 backdrop-blur-sm p-4">(header)</header>
+
+	<!-- Main -->
+	<main class="p-4">
+		<slot></slot>
+	</main>
+
+	<!-- Footer -->
+	<footer class="p-4">(footer)</footer>
+</div>
