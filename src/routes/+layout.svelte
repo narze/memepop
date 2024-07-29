f429a003e32048a93564f55cee257f14138e8c56
Manassarn Manoonchai <manassarn@gmail.com>
2024-07-29 15:45:53 +0700

    Add iframe page for testing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,4 @@
 	import '../app.css';
 </script>
 
-<div class="grid h-dvh grid-rows-[auto_1fr_auto]">
-	<!-- Header -->
-	<header class="sticky top-0 z-10 backdrop-blur-sm p-4">(header)</header>
-
-	<!-- Main -->
-	<main class="p-4">
-		<slot></slot>
-	</main>
-
-	<!-- Footer -->
-	<footer class="p-4">(footer)</footer>
-</div>
+<slot />
